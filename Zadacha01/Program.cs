e2c3a34566625c247135e7d1be63952185cbe96f
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-06-28 03:27:01 +0300

    Add Zadacha 001

--- Zadacha01/Program.cs
+++ Zadacha01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее
+
+int a = 10;
+int b = 2;
+
+int max = a;
+
+if (b > a)
+{
+    max = b;
+}
+Console.Write("Max = ");
+Console.WriteLine(max);
