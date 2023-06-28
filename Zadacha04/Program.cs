9447284118e9c6996b5b0a9367c41e1f8b11adc6
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-06-28 04:05:49 +0300

    Add Zadacha 04

---
+++ Zadacha04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+int N = 24;
+
+Console.Write("Все четные числа от 1 до ");
+Console.WriteLine(N);
+    
+int Num = 1; 
+
+while (Num <= N)
+{
+    if (Num%2 == 0)
+    {
+        Console.Write(Num);
+        Console.Write("  ");
+    }
+    Num ++;
+}
